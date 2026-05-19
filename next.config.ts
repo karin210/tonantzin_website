f2fb37c9b29fff2df7d25520eb3da272c029eae0
Karin <karin_ar72@hotmail.com>
2026-05-19 16:57:42 -0600

    style: use vh instead of dvh for hero section container

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["22ff-206-85-11-81.ngrok-free.app"],
+  allowedDevOrigins: ["fd20-206-85-11-81.ngrok-free.app"],
 };
 
 export default nextConfig;
